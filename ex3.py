014fc1e6e7a2db3f5bb1d926e05e811581c50bab
mvrk <guojiarui@gmail.com>
2014-05-04 02:53:33 -0500

    added five short codes

---
+++ ex3.py
@@ -0,0 +1,16 @@
+print " I will not count my chickens."
+
+print "Hens", 35 + 30/6
+print "Roosters", 100 - 25 * 3/4
+print "Now I will count the eggs:"
+
+print 3+2+1-5+4 % 2 - 1 / 4 + 6
+print "Is it true htat 3+2<5-7?"
+print "What is 3+2?", 3+2
+print "What is 5-7?", 5-7
+
+print "Oh, that is why it's False."
+print "How about some more."
+
+print "Is it grater?", 5>-2
+print "Is it greater or equal?", 5<=-2
---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = 'Alex .R Guo'
+my_age = 25 # This is a lie.
+my_height = 71
+my_weight = 160
+my_eyes = 'brown'
+my_teeth ='white'
+my_hair = 'black'
+
+print " Let's talk about %s." % my_name
+print " He's %d inches tall." % my_height
+print " He's %d pounds heavy." %my_weight
+print " He's got %s eyes and %s hair."%(my_eyes,my_hair)
+print " His teech are usually %s depending on the coffee." % my_teeth
+
+#This line is trickly, try to get it exactlly right.
+print " If I add %d, %d, and %d I get %d. " % ( my_age, my_height, my_weight, my_age + my_height + my_weight)
